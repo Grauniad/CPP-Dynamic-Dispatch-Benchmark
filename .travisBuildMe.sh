fdbe76f9ec2a15797b0bca07a4daa29b78522a62
Luke Humphreys <lhumphreys67@gmail.com>
2013-09-14 17:39:28 +0100

    Move TRAVIS build script into dev_tools

--- .travisBuildMe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -qq
-sudo apt-get install -qq g++-4.8;
-
-TOOL_CHAIN=TRAVIS PROJECT_ROOT_DIR=$PWD ONLY_MODES=CPP make $*
